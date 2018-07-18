da80841850f395ae9fa2e8ac2f39a2f145e06c5d
Ivan Coronado Moreno <ivacormor@gmail.com>
2018-07-18 22:48:41 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import './App.css';
+
+class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        Hellow
+      </div>
+    );
+  }
+}
+
+export default App;
